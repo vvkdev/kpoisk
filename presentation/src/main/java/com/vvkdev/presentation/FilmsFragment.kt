e37b8bb02ee11e7b25876d0ebcf8d16f469c8a01
vvkdev <vvkdeveloper@gmail.com>
2025-04-05 05:58:55 +0300

    add BaseFragment

--- presentation/src/main/java/com/vvkdev/presentation/FilmsFragment.kt
+++ presentation/src/main/java/com/vvkdev/presentation/FilmsFragment.kt
@@ -1,31 +1,12 @@
 package com.vvkdev.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.vvkdev.presentation.databinding.FragmentFilmsBinding
 
-class FilmsFragment : Fragment() {
-
-    private var _binding: FragmentFilmsBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFilmsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class FilmsFragment : BaseFragment<FragmentFilmsBinding>(FragmentFilmsBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
