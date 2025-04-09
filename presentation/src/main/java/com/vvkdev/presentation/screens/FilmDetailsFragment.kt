3531a5ce07ff8bb79d20dad94278f5645d0b527c
vvkdev <vvkdeveloper@gmail.com>
2025-04-09 17:39:06 +0300

    add FilmDetailsFragment

---
+++ presentation/src/main/java/com/vvkdev/presentation/screens/FilmDetailsFragment.kt
@@ -0,0 +1,15 @@
+package com.vvkdev.presentation.screens
+
+import android.os.Bundle
+import android.view.View
+import com.vvkdev.presentation.BaseFragment
+import com.vvkdev.presentation.databinding.FragmentFilmDetailsBinding
+
+class FilmDetailsFragment :
+    BaseFragment<FragmentFilmDetailsBinding>(FragmentFilmDetailsBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
