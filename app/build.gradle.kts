b57a6b6d1ed1be3f6832daa0a1a536fb85db2bfa
vvkdev <vvkdeveloper@gmail.com>
2025-04-09 08:19:39 +0300

    add data module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 dependencies {
     implementation(project(":presentation"))
     implementation(project(":domain"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
---
+++ data/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+android {
+    namespace = "com.vvkdev.data"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jvm.get())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.jvm.get()
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    // hilt
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+    // retrofit
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.converter.kotlinx.serialization)
+    implementation(libs.okhttp)
+}
