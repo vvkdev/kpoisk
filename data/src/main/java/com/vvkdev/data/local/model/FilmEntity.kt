93e403c636f1227540f50ee48ae5394289eeb15b
vvkdev <vvkdeveloper@gmail.com>
2025-04-23 14:43:48 +0300

    add film entity

---
+++ data/src/main/java/com/vvkdev/data/local/model/FilmEntity.kt
@@ -0,0 +1,20 @@
+package com.vvkdev.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "films")
+data class FilmEntity(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val foreignName: String,
+    val rating: Float,
+    val votes: Int,
+    val year: String,
+    val description: String,
+    val poster: String,
+    val length: Int?,
+    val has3D: Boolean,
+    val genres: String,
+    val countries: String,
+)
