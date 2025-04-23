ab6f2cbfc21148b00494e711985b4ca322e01f4f
vvkdev <vvkdeveloper@gmail.com>
2025-04-23 20:43:03 +0300

    rename Main Hilt module to RepositoryModule and move to data layer

--- data/src/main/java/com/vvkdev/data/di/RepositoryModule.kt
+++ data/src/main/java/com/vvkdev/data/di/RepositoryModule.kt
@@ -1,4 +1,4 @@
-package com.vvkdev.kpoisk.di
+package com.vvkdev.data.di
 
 import com.vvkdev.data.repository.FilmsRepositoryImpl
 import com.vvkdev.domain.repository.FilmsRepository
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface MainModule {
+interface RepositoryModule {
 
     @Binds
     fun provideFilmRepository(impl: FilmsRepositoryImpl): FilmsRepository
