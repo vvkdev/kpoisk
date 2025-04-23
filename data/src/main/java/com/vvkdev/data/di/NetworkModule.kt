3c61f6d4dac316d2a0ba10d2a1b4793631ecf5a7
vvkdev <vvkdeveloper@gmail.com>
2025-04-23 16:01:04 +0300

    refactor NetworkModule

---
+++ data/src/main/java/com/vvkdev/data/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.vvkdev.data.di
+
+import com.vvkdev.data.remote.service.FilmsService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.kinopoisk.dev/v1.4/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(json: Json): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideFilmsService(retrofit: Retrofit): FilmsService =
+        retrofit.create(FilmsService::class.java)
+}
