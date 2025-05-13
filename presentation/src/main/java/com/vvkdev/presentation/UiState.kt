5a6d91452eafbf3bcf09078377c99034be727a54
vvkdev <vvkdeveloper@gmail.com>
2025-05-13 13:29:36 +0300

    convert object to data object

--- presentation/src/main/java/com/vvkdev/presentation/UiState.kt
+++ presentation/src/main/java/com/vvkdev/presentation/UiState.kt
@@ -1,8 +1,8 @@
 package com.vvkdev.presentation
 
 sealed class UiState<out T> {
-    object Default : UiState<Nothing>()
-    object Loading : UiState<Nothing>()
+    data object Default : UiState<Nothing>()
+    data object Loading : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
     data class Error(val message: String?) : UiState<Nothing>()
 }
