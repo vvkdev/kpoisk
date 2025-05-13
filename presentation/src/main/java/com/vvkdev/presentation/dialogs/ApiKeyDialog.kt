c4e3843a5b6975b2b326c71d09c0d1d70260d594
vvkdev <vvkdeveloper@gmail.com>
2025-05-13 12:37:39 +0300

    add UiState processing

--- presentation/src/main/java/com/vvkdev/presentation/dialogs/ApiKeyDialog.kt
+++ presentation/src/main/java/com/vvkdev/presentation/dialogs/ApiKeyDialog.kt
@@ -2,15 +2,22 @@ package com.vvkdev.presentation.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.vvkdev.presentation.R
+import com.vvkdev.presentation.UiState
 import com.vvkdev.presentation.databinding.DialogApikeyBinding
 import com.vvkdev.presentation.viewmodels.ApiKeyViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ApiKeyDialog : DialogFragment() {
@@ -28,15 +35,49 @@ class ApiKeyDialog : DialogFragment() {
             .create()
 
         dialog.setOnShowListener {
+            val cancelButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+            val saveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+
+            with(binding) {
 
                 apiKeyEditText.doAfterTextChanged {
                     if (apiKeyEditText.text.toString().isNotBlank()) setCancelable(false)
                 }
 
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                viewModel.saveApiKey(binding.apiKeyEditText.text.toString())
-                Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
-                dialog.dismiss()
+                cancelButton.setOnClickListener { dismiss() }
+
+                saveButton.setOnClickListener { viewModel.saveApiKey(apiKeyEditText.text.toString()) }
+
+                lifecycleScope.launch {
+                    repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        viewModel.state.collect { state ->
+                            when (state) {
+                                UiState.Default -> {}
+                                UiState.Loading -> {
+                                    progressBar.visibility = View.VISIBLE
+                                    apiKeyTextInputLayout.visibility = View.GONE
+                                    saveButton.isEnabled = false
+                                    cancelButton.isEnabled = false
+                                }
+
+                                is UiState.Success -> {
+                                    Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT)
+                                        .show()
+                                    dismiss()
+                                }
+
+                                is UiState.Error -> {
+                                    progressBar.visibility = View.GONE
+                                    apiKeyTextInputLayout.visibility = View.VISIBLE
+                                    apiKeyTextInputLayout.error =
+                                        state.message ?: getString(R.string.unknown_error)
+                                    saveButton.isEnabled = true
+                                    cancelButton.isEnabled = true
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         return dialog
--- presentation/src/main/java/com/vvkdev/presentation/viewmodels/ApiKeyViewModel.kt
+++ presentation/src/main/java/com/vvkdev/presentation/viewmodels/ApiKeyViewModel.kt
@@ -3,7 +3,10 @@ package com.vvkdev.presentation.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vvkdev.domain.repository.ApiKeyRepository
+import com.vvkdev.presentation.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,7 +15,18 @@ class ApiKeyViewModel @Inject constructor(
     private val apiKeyRepository: ApiKeyRepository,
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow<UiState<Unit>>(UiState.Default)
+    val state: StateFlow<UiState<Unit>> = _state
+
     fun saveApiKey(key: String) {
-        viewModelScope.launch { apiKeyRepository.setApiKey(key) }
+        viewModelScope.launch {
+            _state.value = UiState.Loading
+            try {
+                apiKeyRepository.setApiKey(key)
+                _state.value = UiState.Success(Unit)
+            } catch (e: Exception) {
+                _state.value = UiState.Error(e.message)
+            }
+        }
     }
 }
