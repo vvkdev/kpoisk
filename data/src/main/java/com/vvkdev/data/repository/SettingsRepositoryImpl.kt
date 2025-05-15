a0920d2340996a4103a0cfae18e1f9dbfffd079f
vvkdev <vvkdeveloper@gmail.com>
2025-05-15 23:19:27 +0300

    add settings repository methods impl

--- data/src/main/java/com/vvkdev/data/repository/SettingsRepositoryImpl.kt
+++ data/src/main/java/com/vvkdev/data/repository/SettingsRepositoryImpl.kt
@@ -1,14 +1,25 @@
 package com.vvkdev.data.repository
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
+import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.vvkdev.domain.repository.SettingsRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SettingsRepositoryImpl @Inject constructor(
-    private val dataStore: DataStore<Preferences>
+    private val sharedPreferences: SharedPreferences,
 ) : SettingsRepository {
 
+    override fun setColorAccent(color: String) {
+        sharedPreferences.edit { putString(ACCENT_COLOR, color) }
+    }
+
+    override fun getColorAccent(): String =
+        sharedPreferences.getString(ACCENT_COLOR, DEFAULT_COLOR) ?: DEFAULT_COLOR
+
+    companion object {
+        private const val ACCENT_COLOR = "accent_color"
+        private const val DEFAULT_COLOR = "blue"
+    }
 }
