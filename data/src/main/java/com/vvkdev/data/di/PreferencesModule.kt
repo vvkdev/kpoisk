5dd6a1fb8ba1274b9ca914dbd89d39e7ce3981f8
vvkdev <vvkdeveloper@gmail.com>
2025-05-15 23:18:46 +0300

    add Hilt shared prefs module

--- data/src/main/java/com/vvkdev/data/di/PreferencesModule.kt
+++ data/src/main/java/com/vvkdev/data/di/PreferencesModule.kt
@@ -2,6 +2,7 @@ package com.vvkdev.data.di
 
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.Module
 import dagger.Provides
@@ -14,9 +15,15 @@ private val Context.dataStore by preferencesDataStore(name = "app_settings")
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataStoreModule {
+object PreferencesModule {
 
     @Provides
     @Singleton
     fun provideDataStore(@ApplicationContext context: Context) = context.dataStore
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences("app_settings", Context.MODE_PRIVATE)
+    }
 }
