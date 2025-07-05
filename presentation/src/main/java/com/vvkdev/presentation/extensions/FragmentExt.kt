c11b4705d6016b622697ef63a056b057b16ef40e
vvkdev <vvkdeveloper@gmail.com>
2025-07-05 21:39:03 +0300

    add shareText and openInBrowser fragment extensions

--- presentation/src/main/java/com/vvkdev/presentation/extensions/FragmentExt.kt
+++ presentation/src/main/java/com/vvkdev/presentation/extensions/FragmentExt.kt
@@ -1,7 +1,9 @@
 package com.vvkdev.presentation.extensions
 
+import android.content.Intent
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.core.net.toUri
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -12,6 +14,19 @@ import kotlinx.coroutines.launch
 
 fun Fragment.showShortToast(@StringRes messageRes: Int) {
     Toast.makeText(requireContext(), messageRes, Toast.LENGTH_SHORT).show()
+}
+
+fun Fragment.shareText(text: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, text)
+    }
+    startActivity(Intent.createChooser(intent, text))
+}
+
+fun Fragment.openInBrowser(url: String) {
+    val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+    startActivity(intent)
 }
 
 fun <T> Fragment.collectWhenStarted(flow: Flow<T>, action: suspend (T) -> Unit) {
