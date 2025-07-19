3adbda7651abad8b083af9f4a49a5923a7355b9a
vvkdev <vvkdeveloper@gmail.com>
2025-07-19 20:36:47 +0300

    add films SearchView

--- presentation/src/main/java/com/vvkdev/presentation/fragments/FilmsFragment.kt
+++ presentation/src/main/java/com/vvkdev/presentation/fragments/FilmsFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.navigation.fragment.findNavController
 import com.vvkdev.presentation.base.BaseFragment
 import com.vvkdev.presentation.databinding.FragmentFilmsBinding
-import com.vvkdev.presentation.extensions.setOnDoneAction
 
 class FilmsFragment : BaseFragment<FragmentFilmsBinding>() {
 
@@ -17,14 +17,21 @@ class FilmsFragment : BaseFragment<FragmentFilmsBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.openButton.setOnClickListener { openFilm() }
-        binding.filmIdEditText.setOnDoneAction { openFilm() }
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query.isNotBlank()) openFilm(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return true
+            }
+        })
     }
 
-    private fun openFilm() {
-        val filmId = binding.filmIdEditText.text.toString().toIntOrNull() ?: 0
+    private fun openFilm(filmId: String) {
         val direction =
-            FilmsFragmentDirections.actionFilmsFragmentToFilmFragment(filmId)
+            FilmsFragmentDirections.actionFilmsFragmentToFilmFragment(filmId.toInt())
         findNavController().navigate(direction)
     }
 }
