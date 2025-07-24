6b7dd9a5b0f6e03497803723932a49bf5cb3e393
vvkdev <vvkdeveloper@gmail.com>
2025-07-24 21:44:18 +0300

    add FilmsAdapter

---
+++ presentation/src/main/java/com/vvkdev/presentation/adapters/FilmsAdapter.kt
@@ -0,0 +1,32 @@
+package com.vvkdev.presentation.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vvkdev.presentation.databinding.ItemFilmBinding
+import com.vvkdev.presentation.model.FilmItem
+import com.vvkdev.presentation.viewholders.FilmItemViewHolder
+
+class FilmsAdapter(private val onRootClickListener: (FilmItem) -> Unit) :
+    RecyclerView.Adapter<FilmItemViewHolder>() {
+
+    var items: List<FilmItem> = emptyList()
+        set(newValue) {
+            field = newValue
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFilmBinding.inflate(inflater, parent, false)
+        return FilmItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FilmItemViewHolder, position: Int) {
+        val film = items[position]
+        holder.bind(film)
+        holder.itemView.setOnClickListener { onRootClickListener(film) }
+    }
+
+    override fun getItemCount() = items.size
+}
