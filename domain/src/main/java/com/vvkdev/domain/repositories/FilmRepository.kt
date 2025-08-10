6e68ba673c264fa988f29fc012981eb30abf3230
vvkdev <vvkdeveloper@gmail.com>
2025-08-10 21:31:19 +0300

    remove linebreak

--- domain/src/main/java/com/vvkdev/domain/repositories/FilmRepository.kt
+++ domain/src/main/java/com/vvkdev/domain/repositories/FilmRepository.kt
@@ -5,6 +5,5 @@ import com.vvkdev.domain.models.FilmShort
 
 interface FilmRepository {
     suspend fun getFilmById(id: Int, forceRefresh: Boolean): Result<Film>
-
     suspend fun findByName(name: String): Result<List<FilmShort>>
 }
